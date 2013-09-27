5522efcd684845211df7097d6fb2110bc6c1ac7a
Michael <me@michael-kuck.com>
2013-09-27 12:39:19 +0800

    MKCommons v1.0.1 - Fixed copy headers build step to include all single headers for the different parts

--- MKCommons/MKVersion.h
+++ MKCommons/MKVersion.h
@@ -14,12 +14,15 @@ extern NSUInteger const PATCH;
 
 NSUInteger const MAJOR = 1;
 NSUInteger const MINOR = 0;
-NSUInteger const PATCH = 0;
+NSUInteger const PATCH = 1;
 
 /*
  * ********************************
  * MKCommons Changelog
  * ********************************
+ *
+ * v1.0.1
+ * - Fixed copy headers build step to include all single headers for the different parts
  *
  * v1.0.0
  * - Initial commit containing: MKFallbackQueue, MKMiscHelper, MKSerialManager and MKStopwatch.
