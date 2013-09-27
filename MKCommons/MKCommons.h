710275d272cc105b33723af756a50b8eabfb3672
Michael <me@michael-kuck.com>
2013-09-27 10:34:53 +0800

    Initial Commit

---
+++ MKCommons/MKCommons.h
@@ -0,0 +1,13 @@
+//
+//  MKCommons.h
+//  MKCommons
+//
+//  Created by Michael Kuck on 9/27/13.
+//  Copyright (c) 2013 Michael Kuck. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MKCommons : NSObject
+
+@end
