f63dc818ee33c5d63dff8d8a0c06ebf8941a222c
MichaelK <me@michael-kuck.com>
2014-08-20 17:58:23 +0800

    Added MKUITheme protocol and UIParallaxImageView (not implemented yet).

---
+++ MKCommons/UIParallaxImageView.h
@@ -0,0 +1,11 @@
+//
+// Created by Michael Kuck on 8/20/14.
+// Copyright (c) 2014 Michael Kuck. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UIParallaxImageView : UIView
+
+
+@end
---
+++ MKCommons/UIParallaxImageView.m
@@ -0,0 +1,12 @@
+//
+// Created by Michael Kuck on 8/20/14.
+// Copyright (c) 2014 Michael Kuck. All rights reserved.
+//
+
+#import "UIParallaxImageView.h"
+
+@implementation UIParallaxImageView
+{
+
+}
+@end
