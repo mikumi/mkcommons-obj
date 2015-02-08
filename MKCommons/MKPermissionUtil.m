f744a24e94d1322090e5d751a8b9250e1182aa39
MichaelK <me@michael-kuck.com>
2015-02-08 13:16:33 +0800

    Add permission util for easy handling of iOS permissions

---
+++ MKCommons/MKPermissionUtil.m
@@ -0,0 +1,101 @@
+//
+// Created by Michael Kuck on 1/29/15.
+// Copyright (c) 2015 Michael Kuck. All rights reserved.
+//
+
+#import "MKPermissionUtil.h"
+#import "MKPreferencesManager.h"
+#import "MKSystemHelper.h"
+#import "UIAlertView+MKCommons.h"
+
+static NSString *const DidAskPermissionKeyPrefix = @"DidAskPermission";
+
+//============================================================
+//== Private Interface
+//============================================================
+@interface MKPermissionUtil ()
+
+@property (nonatomic, strong) UIAlertView *alert;
+
+- (NSString *)permissionKeyForType:(MKPermissionType)permissionType;
+- (void)storePermission:(BOOL)isPermitted forPermissionType:(MKPermissionType)permissionType;
+- (void)requestNativePermission:(MKPermissionType)permissionType;
+
+@end
+
+//============================================================
+//== Implementation
+//============================================================
+@implementation MKPermissionUtil
+
+- (BOOL)hasAskedPermission:(MKPermissionType)permissionType
+{
+    if (permissionType == MKPermissionTypePushNotifications && [MKSystemHelper isOS7OrLessPlatform]) {
+        return YES;
+    }
+    NSString *const preferencesKey = [self permissionKeyForType:permissionType];
+    BOOL const hasAsked = [[MKPreferencesManager defaultManager] boolForKey:preferencesKey];
+    return hasAsked;
+}
+
+- (void)askForPermission:(MKPermissionType)permissionType title:(NSString *)title message:(NSString *)message
+                yesTitle:(NSString *)yesTitle noTitle:(NSString *)noTitle
+              completion:(MKPermissionUtilAskCompletion)completion
+{
+    if ([self hasAskedPermission:permissionType]) {
+        if (completion) {
+            completion(YES);
+        }
+        return;
+    }
+
+    self.alert = [[UIAlertView alloc]
+            initWithTitle:title message:message delegate:self cancelButtonTitle:noTitle
+            otherButtonTitles:yesTitle, nil];
+    [self.alert showWithCompletion:^(NSInteger buttonIndex) {
+        self.alert           = nil;
+        BOOL const didPermit = (buttonIndex == 1);
+        [self storePermission:didPermit forPermissionType:permissionType];
+        if (didPermit) {
+            [self requestNativePermission:permissionType];
+        }
+    }];
+}
+
+#pragma mark - Private Implementation
+
+- (NSString *)permissionKeyForType:(MKPermissionType)permissionType
+{
+    return [NSString stringWithFormat:@"%@-%zd", DidAskPermissionKeyPrefix, permissionType];
+}
+
+- (void)storePermission:(BOOL)isPermitted forPermissionType:(MKPermissionType)permissionType
+{
+    NSString             *const preferencesKey     = [self permissionKeyForType:permissionType];
+    MKPreferencesManager *const preferencesManager = [MKPreferencesManager defaultManager];
+    [preferencesManager setBool:isPermitted forKey:preferencesKey];
+    [preferencesManager synchronize];
+}
+
+- (void)requestNativePermission:(MKPermissionType)permissionType
+{
+    switch (permissionType) {
+        case MKPermissionTypePushNotifications:
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "UnavailableInDeploymentTarget"
+            if ([[UIApplication sharedApplication] respondsToSelector:@selector(registerUserNotificationSettings:)]) {
+                [[UIApplication sharedApplication]
+                        registerUserNotificationSettings:[UIUserNotificationSettings settingsForTypes:
+                                        UIUserNotificationTypeAlert | UIUserNotificationTypeBadge |
+                                        UIUserNotificationTypeSound
+                                categories:nil]];
+            }
+#pragma clang diagnostic pop
+            break;
+
+        default:
+            break;
+    }
+}
+
+@end
